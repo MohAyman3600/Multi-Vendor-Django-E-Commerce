ed36def76c1a736a1ddd9f3ef3b328ee06047ffe
Mohamed Ayman <moh.ayman.3600@gmail.com>
2020-12-31 23:52:57 +0200

    Added pages app

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import HomePageView
+"""Main Pages URL configrtions."""
+from django.urls import path, re_path
+from .views import HomePageView, ErrorPageView
+
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    re_path(r'error/(?P<error_name>.+?)/(?P<error_msg>.+?)/$',
+            ErrorPageView.as_view(), name='error'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,30 @@
+"""Main pages views."""
+from datetime import datetime, timedelta
 from django.views.generic import TemplateView
 
 
+from products.models import Product
+from cart.forms import CartAddProductForm
+
+
 class HomePageView(TemplateView):
+    """Home Template View for displaying home page."""
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        """
+        Add featured products and products added since last week to context.
+        """
+        context = super().get_context_data(**kwargs)
+        context['featured_products'] = Product.objects.filter(is_featured=True)
+        last_week_date = datetime.now() - timedelta(weeks=1)
+        context['latest_products'] = Product.objects.filter(
+            add_date__gt=last_week_date)
+        context['cart_add_form'] = CartAddProductForm()
+        return context
+
+
+class ErrorPageView(TemplateView):
+    """Error Page view."""
+
+    template_name = 'error.html'
